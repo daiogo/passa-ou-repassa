78b9d74a21288b3ae69988c47458a43e29b2f820
Diogo Freitas <dggf.93@gmail.com>
2018-09-30 10:33:01 -0300

    Added application layer file structure

---
+++ code/PassaOuRepassa/app/inc/PassaOuRepassa.h
@@ -0,0 +1,18 @@
+/*
+ * PassaOuRepassa.h
+ *
+ *  Created on: 30 de set de 2018
+ *      Author: diogo
+ */
+
+#ifndef APP_INC_PASSAOUREPASSA_H_
+#define APP_INC_PASSAOUREPASSA_H_
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+void POR_Init(void);
+
+void POR_Run(void);
+
+#endif /* APP_INC_PASSAOUREPASSA_H_ */
---
+++ code/PassaOuRepassa/app/src/PassaOuRepassa.c
@@ -0,0 +1,16 @@
+/*
+ * PassaOuRepassa.c
+ *
+ *  Created on: 30 de set de 2018
+ *      Author: diogo
+ */
+
+void POR_Init(void)
+{
+
+}
+
+void POR_Run(void)
+{
+
+}
