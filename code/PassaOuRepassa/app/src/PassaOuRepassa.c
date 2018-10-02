a727ed52eb3e4b1d282f331ab6be06567372eb0a
Diogo Freitas <dggf.93@gmail.com>
2018-10-02 19:47:31 -0300

    First implementation of the remaining necessary peripherals

--- code/PassaOuRepassa/app/src/PassaOuRepassa.c
+++ code/PassaOuRepassa/app/src/PassaOuRepassa.c
@@ -17,37 +17,58 @@ void POR_Init(void)
 {
     // Board hardware init
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);   // Set up the clock
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);                                        // Enable port D
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);                                        // Enable port E
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);                                        // Enable port F
 
-    // Configure switch 1
-    POR_InitSwitch(GPIO_PORTF_BASE, GPIO_PIN_4);
+    // Configure switches
+    POR_InitSwitch(GPIO_PORTD_BASE, GPIO_PIN_0);
+    POR_InitSwitch(GPIO_PORTD_BASE, GPIO_PIN_1);
+    POR_InitSwitch(GPIO_PORTD_BASE, GPIO_PIN_2);
+    POR_InitSwitch(GPIO_PORTD_BASE, GPIO_PIN_3);
 
-    // Configure red led
+    // Configure LEDs
+    POR_InitLed(GPIO_PORTE_BASE, GPIO_PIN_1);
+    POR_InitLed(GPIO_PORTE_BASE, GPIO_PIN_2);
+    POR_InitLed(GPIO_PORTE_BASE, GPIO_PIN_3);
     POR_InitLed(GPIO_PORTF_BASE, GPIO_PIN_1);
 }
 
 void POR_Run(void)
 {
+
     while (1)
     {
-        if (POR_ReadFromSwitch(GPIO_PORTF_BASE, GPIO_PIN_4) == GPIO_PIN_4)
+        if (POR_ReadFromSwitch(GPIO_PORTD_BASE, GPIO_PIN_0) == GPIO_PIN_0)
         {
-            POR_WriteToLed(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
+            POR_WriteToLed(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_PIN_1);
         }
-        else
+        else if (POR_ReadFromSwitch(GPIO_PORTD_BASE, GPIO_PIN_1) == GPIO_PIN_1)
+        {
+            POR_WriteToLed(GPIO_PORTE_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        }
+        else if (POR_ReadFromSwitch(GPIO_PORTD_BASE, GPIO_PIN_2) == GPIO_PIN_2)
+        {
+            POR_WriteToLed(GPIO_PORTE_BASE, GPIO_PIN_3, GPIO_PIN_3);
+        }
+        else if (POR_ReadFromSwitch(GPIO_PORTD_BASE, GPIO_PIN_3) == GPIO_PIN_3)
         {
             POR_WriteToLed(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
         }
+        else
+        {
+            POR_WriteToLed(GPIO_PORTE_BASE, GPIO_PIN_1, 0);
+            POR_WriteToLed(GPIO_PORTE_BASE, GPIO_PIN_2, 0);
+            POR_WriteToLed(GPIO_PORTE_BASE, GPIO_PIN_3, 0);
+            POR_WriteToLed(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
+        }
+
     }
 }
 
 void POR_InitSwitch(uint32_t port, uint8_t pins)
 {
-    // Make the pin a input
-    HAL_GPIO_SetPinDirection(port, pins, GPIO_DIR_MODE_IN);
-
-    // Set the pad for standard push-pull with weak pull-up
-    HAL_GPIO_PadConfigSet(port, pins, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    HAL_GPIO_SetPinAsInput(port, pins);
 }
 
 void POR_InitLed(uint32_t port, uint8_t pins)
